9f8d8fda5f48cab71a426f306baf0373914dfbd4
Gratcy <gratcypalma@gmail.com>
2017-08-06 16:13:13 +0700

    member since

--- routes/login.js
+++ routes/login.js
@@ -23,7 +23,11 @@ exports.login = function(req,res) {
 			connection.query("SELECT * FROM users_tab WHERE uemail='"+input.uemail+"' AND upass='"+helpers.__hash_password(input.upass)+"' AND ustatus=1",function(err, rows) {
 				if (rows[0]) {
 						connection.query("SELECT a.uname as perm,a.uurl as url,b.uaccess as access from users_permission_tab a JOIN users_access_tab b ON a.uid=b.upid WHERE b.ugid="+rows[0].ugid,function(err, perms) {
-						sauth.login = {uid: rows[0].uid, ugroup: rows[0].ugid, uemail: rows[0].uemail, perms: perms};
+						var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
+						var member_since = new Date(rows[0].ucreated * 1000);
+						member_since = monthNames[member_since.getMonth()] + ' ' + member_since.getFullYear();
+						
+						sauth.login = {uid: rows[0].uid, ucreated:member_since, ugroup: rows[0].ugid, uemail: rows[0].uemail, perms: perms};
 						
 						if (err) {
 							console.log("Error Selecting : %s ",err );
